829e076f6d725dc51fb882ab849a6913264184b9
siva <byteshiva@gmail.com>
2019-03-31 16:12:17 +0530

    update sample test case to include intl component

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { IntlProvider } from "react-intl";
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+
+  ReactDOM.render(<IntlProvider locale="en" timeZone="Asia/Tokyo">
+<App /></IntlProvider>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
