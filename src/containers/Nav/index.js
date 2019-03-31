fff7edd98cef2162cbaed13c175f5feb9b0dbb56
siva <byteshiva@gmail.com>
2019-03-31 16:04:52 +0530

    Add seperate Home Page route to handle root path, NoMatch if no route match, update Nav page, Bug: Fix Route by adding Switch Construct

--- src/containers/Nav/index.js
+++ src/containers/Nav/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container, Divider } from "semantic-ui-react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Label } from "semantic-ui-react";
 import Orders from "../../components/Orders/";
 import Profile from "../../components/Profile/Profile";
 import OrderDetails from "../../components/OrderDetails/";
 import HeaderMenu from "../Header";
+import HomePage from '../Home';
 import GB_BASE_URL from '../../constants/base/';
+import NoMatch from '../NoMatch/';
 
 function PreProcessProductDetails(match) {
   return (
@@ -20,16 +22,23 @@ function PreProcessProductDetails(match) {
     </div>
   );
 }
+/** 
+  @todo - Restructure router tag - testing todo Bot
+  @body - expand and include multiple other options, - i'm testing @todo tag - ignore this comment.
+*/
 
 const FixedMenuLayout = () => (
   <Router>
     <Container>
       <HeaderMenu />
       <Container text style={{ marginTop: "7em" }}>
-        <Route exact path={`${GB_BASE_URL}`} component={Profile} />
-        <Route exact path={`${GB_BASE_URL}/profile`} component={Profile} />
-        <Route exact path={`${GB_BASE_URL}/orders`} component={Orders} />
-        <Route path={`${GB_BASE_URL}/orders/:id`} component={PreProcessProductDetails} />
+        <Switch>
+          <Route exact path={`${GB_BASE_URL}/profile`} component={Profile} />
+          <Route exact path={`${GB_BASE_URL}/orders`} component={Orders} />
+          <Route path={`${GB_BASE_URL}/orders/:id`} component={PreProcessProductDetails} />
+          <Route exact path={`${GB_BASE_URL}`} component={HomePage} />
+          <Route component={NoMatch}/>
+        </Switch>
       </Container>
     </Container>
   </Router>
